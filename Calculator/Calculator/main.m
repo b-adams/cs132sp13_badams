eb4534a5600cb86b7963db9aa080d309431cf5d6
Prof. Adams <badams@Mac110-Eldrazi.local>
2013-01-30 16:37:33 -0500

    Added user interface to interact with a calculator.

--- Calculator/Calculator/main.m
+++ Calculator/Calculator/main.m
@@ -7,14 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Calculator.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        char userInput;
+        Calculator* ti84 = nil;
+        ti84 = [[Calculator alloc] init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        while(YES)
+        {
+            printf("Press a key:");
+            scanf(" %c", &userInput);
+            [ti84 pressAKey:userInput];
+            printf("Input: %c. Display: %d\n",
+                   userInput, [ti84 numberOnTheScreen]);
+        }
+        
         
     }
     return 0;
