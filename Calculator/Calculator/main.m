807ef2e4aeca7d1c85e6352adec8055c4c21daa5
Prof. Adams <badams@Mac110-Eldrazi.local>
2013-01-30 16:30:03 -0500

    Creating base project file

---
+++ Calculator/Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Prof. Adams on 1/30/13.
+//  Copyright (c) 2013 Prof. Adams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
